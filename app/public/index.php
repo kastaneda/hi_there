f459261455150ea7eef0ede46d83d53fa8bfc22a
Dmitry Kolesnikov <kastaneda@gmail.com>
2022-05-14 14:18:07 +0300

    Use Alpine, use Redis

--- app/public/index.php
+++ app/public/index.php
@@ -1,9 +1,15 @@
 <?php
 
-$m = new Memcached();
-$m->addServer('memcached', 11211);
-$m->setOption(\Memcached::OPT_BINARY_PROTOCOL, TRUE); // required because of bug
+$r = new \Redis();
+$r->connect('redis');
 
-echo '<h1>Page load counter: ' . $m->increment('counter', 1, 0) . '</h1>';
+//$m = new \Memcached();
+//$m->addServer('memcached', 11211);
+//$m->setOption(\Memcached::OPT_BINARY_PROTOCOL, TRUE);
+
+echo '<h1>Page load counter: '
+    . ' redis=' . $r->incr('counter')
+//  . ' memcached=' .  $m->increment('counter', 1, 0)
+    . '</h1>';
 
 show_source(__FILE__);
